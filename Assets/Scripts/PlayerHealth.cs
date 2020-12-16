a8df0ebffec209172eb764539389d49f8a6d3838
maikytech <maikytech1307@gmail.com>
2020-12-16 15:31:47 -0500

    [Add] Player Health Script

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int startingHealth;
+    [SerializeField] private int currentHealth;
+
+    [SerializeField] private bool isDead;
+    private bool damaged;
+    private Animator playerAnimator;
+    private PlayerMovementController playerMovement;
+
+    private void Awake()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovementController>();
+    }
+
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamaged()
+    {
+        
+        //currentHealth -= damage;
+
+        if (currentHealth <= 0 && !isDead)
+            Death();
+    }
+
+    void Death()
+    {
+        isDead = true;
+        playerMovement.enabled = false;   
+    }
+
+    private void Update()
+    {
+        TakeDamaged();
+    }
+}
