48bbd33414e0a45437dea65d7eef384258db3d35
maikytech <maikytech1307@gmail.com>
2020-12-15 23:08:56 -0500

    [Add] Enemy One

---
+++ Assets/Scripts/EnemyOneMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyOneMovement : MonoBehaviour
+{
+    private Transform playerTransform;
+    private NavMeshAgent naveMeshEnemy;
+
+    private void Awake()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        naveMeshEnemy = GetComponent<NavMeshAgent>();
+    }
+
+    private void Follow()
+    {
+        naveMeshEnemy.SetDestination(playerTransform.position);
+    }
+
+    private void Update()
+    {
+        Follow();
+    }
+}
